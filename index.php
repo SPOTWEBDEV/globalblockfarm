80da9733f16a55b6acaccf62ab63e226d72a40f5
unknown <arumkingsley49@gmail.com>
2025-01-24 09:51:23 +0100

    working-Arumweb

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<title>Homepage &#8211; GetTrade</title>
+	<title>Homepage &#8211; Globalblockfarm</title>
 	<meta name='robots' content='max-image-preview:large' />
 	<style>
 		img:is([sizes="auto" i], [sizes^="auto," i]) {
@@ -1575,7 +1575,7 @@
 						<div class="elementor-element elementor-element-0f96f60 elementor-align-justify elementor-widget elementor-widget-button" data-id="0f96f60" data-element_type="widget" data-widget_type="button.default">
 							<div class="elementor-widget-container">
 								<div class="elementor-button-wrapper">
-									<a class="elementor-button elementor-button-link elementor-size-lg" href="#">
+									<a class="elementor-button elementor-button-link elementor-size-lg" href="./app/login.php">
 										<span class="elementor-button-content-wrapper">
 											<span class="elementor-button-icon">
 												<i aria-hidden="true" class="fas fa-chevron-circle-right"></i> </span>
