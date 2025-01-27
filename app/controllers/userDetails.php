d1ebb5e2d05be6f2f48724e1c54071d709f65c35
SPOTWEBDEV <spotwebdev.com@gmail.com>
2025-01-27 04:53:26 +0100

    work on the userDetails

--- app/controllers/userDetails.php
+++ app/controllers/userDetails.php
@@ -3,5 +3,11 @@
     $fetchUser = mysqli_query($connection, "SELECT * FROM `users` WHERE `id` = '$id'");
     // $fetchKyc = mysqli_query($connection, "SELECT * FROM `kyc` WHERE `user_id` = '$id'");
     // $kycDetails = mysqli_fetch_assoc($fetchKyc);
-    $userDetails = mysqli_fetch_assoc($fetchUser);
+
+    if(mysqli_num_rows($fetchUser)){
+      $userDetails = mysqli_fetch_assoc($fetchUser); 
+    }else{
+     echo "<script> window.location.href = './login.php' </script>";
+    }
+    
 ?>
